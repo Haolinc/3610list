c21fb95fbb8faab235ba34250d03630450c21558
Haolinc <42785899+Haolinc@users.noreply.github.com>
2021-05-15 18:50:21 -0400

    Add files via upload

---
+++ lab5.js
@@ -0,0 +1,26 @@
+var hyena = new Audio("./hyena.mp3");
+var lemur = new Audio("./lemur4.mp3");
+var leopard = new Audio("./leopard7.mp3");
+var lion = new Audio("./lioncub2.mp3");
+var baligator = new Audio("./baligator3.mp3");
+
+
+function playHyena(){
+  hyena.play();
+}
+
+function playlemur(){
+  lemur.play();
+}
+
+function playleopard(){
+  leopard.play();
+}
+
+function playlion(){
+  lion.play();
+}
+
+function playbaligator(){
+  baligator.play();
+}
